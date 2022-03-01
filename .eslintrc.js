7eef3f5f6daff428558344753d6acb3138312185
Alfonso Delgado <ajdfdelgado@gmail.com>
2022-03-01 12:12:27 -0600

    Installing ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+	 "jest": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "settings":{
+        "react": {
+            "version": "detect",
+        },
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    }
+}
